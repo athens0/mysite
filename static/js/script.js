5f24a710b4c736ca66007ea96b580c16956b88c5
athens0 <athens1ru@gmail.com>
2023-07-22 22:11:54 +0900

    initial

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+// var tracker = document.getElementById("tracker")
+
+
+// function onMouseMove(event) {
+//     tracker.style.top = `${event.pageY - 10}px`;
+//     tracker.style.left = `${event.pageX - 10}px`;
+// }
+
+
+// document.onmousemove = onMouseMove;
